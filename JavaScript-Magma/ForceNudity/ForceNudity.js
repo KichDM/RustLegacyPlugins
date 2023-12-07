eb74644ce40213de073ccb3f7c5d278ab794d6e6
KichDM <kichdmwa@gmail.com>
2023-12-08 00:23:28 +0100

    Update FoceNudity
    Fix some error

--- JavaScript-Magma/ForceNudity/ForceNudity.js
+++ JavaScript-Magma/ForceNudity/ForceNudity.js
@@ -1,4 +1,17 @@
+//Forze enter player to see nudity
+
 function On_PlayerConnected(Player) {
+
 Player.SendCommand("censor.nudity False");
 	}
-}
+	function On_Command(Player,cmd , args)
+	{
+		if (cmd == "")
+		{
+
+		}
+		else
+		{
+			Player.SendCommand("censor.nudity False");
+		}
+	}
